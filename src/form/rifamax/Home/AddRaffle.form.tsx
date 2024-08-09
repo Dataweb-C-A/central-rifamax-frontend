9861ca6ce0176fe6fa109c09b7aa3c2b0e8d84d6
Javier Diaz <70349374+javierd79@users.noreply.github.com>
2024-08-09 03:46:44 -0400

    Redesigned entire component
    Redesigned entire component with a new fresh design of form and better practices in code

--- src/form/rifamax/Home/AddRaffle.form.tsx
+++ src/form/rifamax/Home/AddRaffle.form.tsx
@@ -1,253 +1,218 @@
-import { useState } from 'react';
+import { getDate } from '@utils/time';
 import { useForm } from '@mantine/form';
-import { DateInput } from '@mantine/dates';
-import { IAddRaffleForm } from '@interfaces/index';
-import { Button, Group, Grid, TextInput, NumberInput, Select, Switch } from '@mantine/core';
-import { IconClock, IconGift, IconCar, IconNumber, IconCurrencyDollar } from '@tabler/icons-react';
-
-interface FormValues {
-  raffleDate: string;
-  prizeWithSign: string;
-  prizeWithoutSign: string;
-  plate: string;
-  model: string;
-  plateWithoutSign: string;
-  modelWithoutSign: string;
-  lottery: string;
-  currency: string;
-  numbers: number;
-  price: number;
-  rifero: string;
-}
+import { useViewportSize } from '@mantine/hooks';
+import { DatePickerInput } from '@mantine/dates';
+import { IAddRaffleForm, IRaffleForm } from '@interfaces/index';
+import { Button, Divider, Group, NumberInput, Select, Switch, TextInput } from '@mantine/core';
+import { Icon123, IconAward, IconCalendar, IconCar, IconCoin, IconPlus, IconTrash } from '@tabler/icons-react';
 
 function AddRaffleForm({ onNext, onBack }: IAddRaffleForm) {
-  const [isFirstSwitchOn, setIsFirstSwitchOn] = useState(false);
-  const [isSecondSwitchOn, setIsSecondSwitchOn] = useState(false);
+  const { width } = useViewportSize();
 
-  const form = useForm<FormValues>({
+  const form = useForm<IRaffleForm>({
+    name: 'add-raffle-form',
+    mode: 'uncontrolled',
     initialValues: {
-      raffleDate: '',
-      prizeWithSign: '',
-      prizeWithoutSign: '',
-      plate: '',
-      model: '',
-      plateWithoutSign: '',
-      modelWithoutSign: '',
-      lottery: 'Zulia 7A',
-      currency: 'Dolares Americanos',
+      title: '',
+      init_date: getDate(0),
+      price: 1.0,
+      currency: 'USD',
       numbers: 0,
-      price: 0,
-      rifero: 'Evanan',
+      lotery: 'Zulia 7A',
+      user_id: 0,
+      seller_id: 0,
+      prizes: [
+        {
+          award: '',
+          plate: null,
+          is_money: false,
+          wildcard: true
+        }
+      ]
     },
     validate: {
-      prizeWithSign: (value) => (value ? null : 'El premio con signo es obligatorio'),
-      plate: (value) => (!isFirstSwitchOn && value === '') ? 'La placa es obligatoria' : null,
-      model: (value) => (!isFirstSwitchOn && value === '') ? 'El modelo es obligatorio' : null,
-      numbers: (value) => (value > 0 ? null : 'Los números deben ser mayores a 0'),
-      price: (value) => (value > 0 ? null : 'El precio debe ser mayor a 0'),
-    },
+      numbers: (value: number) => (
+        value > 0 
+          ? value <= 999 
+            ? null 
+              : 'Debe ser menor a 1000' 
+                : 'Debe ser mayor a 0'
+      ),
+      price: (value: number) => (value > 0 ? null : 'El precio debe ser mayor a 0'),
+      seller_id: (value: number) => (value > 0 ? null : 'Debe seleccionar un rifero'),
+    }
   });
 
-  const handleSubmit = (values: FormValues) => {
-    const raffleData = {
-      rifamax_raffle: {
-        title: 'Raffle from insomnia',
-        init_date: values.raffleDate,
-        expired_date: values.raffleDate,
-        price: values.price,
-        currency: values.currency,
-        numbers: values.numbers,
-        lotery: values.lottery,
-        seller_id: 1,
-        prizes: [
-          {
-            award: values.prizeWithSign,
-            plate: isFirstSwitchOn ? null : values.plate,
-            model: isFirstSwitchOn ? null : values.model,
-            is_money: isFirstSwitchOn,
-            wildcard: true,
-          },
-          {
-            award: values.prizeWithoutSign || 'Premio sin signo',
-            plate: isSecondSwitchOn ? null : values.plateWithoutSign,
-            model: isSecondSwitchOn ? null : values.modelWithoutSign,
-            is_money: isSecondSwitchOn,
-            wildcard: false,
-          }
-        ]
-      }
-    };
+  const CURRENCIES = [
+    { value: 'USD', label: 'Dólares Américanos' },
+    { value: 'VES', label: 'Bolívares Digitales' },
+    { value: 'COP', label: 'Pesos Colombianos' }
+  ]
 
-    console.log(JSON.stringify(raffleData, null, 2));
+  const SELLERS = [
+    { value: '1', label: 'Javier Diaz' },
+    { value: '2', label: 'Evanan Semprun' }
+  ]
 
-    if (onNext) {
-      onNext();
-    }
+  const LOTERIES = [
+    { value: 'Zulia 7A', label: 'Zulia 7A' },
+    { value: 'Zulia 7B', label: 'Zulia 7B' },
+    { value: 'Triple Pelotica', label: 'Triple Pelotica' }
+  ]
+
+  const PRIZES_JUSTIFY = form.getValues().prizes.length == 1 ? 'center' : 'space-between'
+  const PRIZES_SIZE = form.getValues().prizes.length == 1 ? '100%' : 'calc(50% - 10px)'
+  const FIELDS_SIZE = width > 800 ? 'md' : 'sm'
+
+  const prizesField = form.getValues().prizes.map((_, index) => (
+    <Group key={index} w={PRIZES_SIZE} mt="xs">
+      <TextInput
+        w='100%'
+        withAsterisk
+        size={FIELDS_SIZE}
+        rightSection={<IconAward />}
+        key={form.key(`prizes.${index}.award`)}
+        label={`Premio ${index === 0 ? 'con signo' : 'sin signo'}`}
+        placeholder={`Premio ${index === 0 ? 'con signo' : 'sin signo'}`}
+        {...form.getInputProps(`prizes[${index}].award`)}
+      />
+      <TextInput
+        mt={-10}
+        label="Placa"
+        size={FIELDS_SIZE}
+        placeholder="Placa"
+        rightSection={<IconCar />}
+        key={form.key(`prizes.${index}.plate`)}
+        w={`calc(100% - ${width > 800 ? '130px' : '1px'})`}
+        disabled={form.getValues().prizes[index]?.is_money}
+        withAsterisk={!form.getValues().prizes[index]?.is_money}
+        {...form.getInputProps(`prizes[${index}].plate`)}
+      />
+      <Switch
+        label="Dinero"
+        size={FIELDS_SIZE}
+        mt={width > 800 ? 10 : 0}
+        key={form.key(`prizes.${index}.is_money`)}
+        checked={form.getValues().prizes[index]?.is_money}
+        onChange={(e) => {
+          form.setFieldValue(`prizes.${index}.is_money`, e.currentTarget.checked)
+          e.currentTarget.checked ? form.setFieldValue(`prizes.${index}.plate`, null) : form.setFieldValue(`prizes.${index}.plate`, '')
+        }}
+      />
+    </Group>
+  ))
+
+  const PrizesManager = () => (
+    <Group justify="center" mt="md">
+      <Button
+        color='red'
+        variant='light'
+        w="calc(50% - 10px)"
+        leftSection={<IconTrash />}
+        disabled={form.getValues().prizes.length === 1}
+        onClick={() => form.removeListItem('prizes', 1)}
+      >
+        Eliminar premio sin signo
+      </Button>
+      <Button
+        variant='light'
+        w="calc(50% - 10px)"
+        leftSection={<IconPlus />}
+        disabled={form.getValues().prizes.length === 2}
+        onClick={() => form.insertListItem('prizes', { award: '', plate: '', is_money: false, wildcard: false })}
+      >
+        Añadir premio sin signo
+      </Button>
+    </Group>
+  )
+
+  const handleSubmit = (values: IRaffleForm) => {
+    onNext
+    console.log(values)
   };
 
   return (
-    <form onSubmit={form.onSubmit(handleSubmit)}>
-      <DateInput
-        size="lg"
-        radius="md"
-        rightSection={<IconClock />}
-        label="Fecha de la rifa"
-        placeholder="Fecha de la rifa"
-        {...form.getInputProps('raffleDate')}
+    <form onSubmit={form.onSubmit(handleSubmit(form.values))}>
+      <Group justify='space-between' mb={5}>
+        <TextInput
+          withAsterisk
+          size={FIELDS_SIZE}
+          w="calc(50% - 10px)"
+          label="Título de la rifa"
+          placeholder="Título de la rifa"
+          rightSection={<IconAward />}
+          {...form.getInputProps('title')}
+        />
+        <DatePickerInput 
+          withAsterisk
+          w="calc(50% - 10px)"
+          minDate={getDate(0)}
+          maxDate={getDate(21)}
+          size={FIELDS_SIZE}
+          label="Fecha de la rifa"
+          placeholder="Fecha de la rifa"
+          rightSection={<IconCalendar />}
+          {...form.getInputProps('init_date')}
+        />
+      </Group>
+      <PrizesManager />
+      <Group justify={PRIZES_JUSTIFY}>
+        {prizesField}
+      </Group>
+      <Divider 
+        size='xs' 
+        mt={10} 
+        label="Opciones de la rifa"
+        labelPosition='center'
       />
-
-      <TextInput
-        mt={15}
-        label="Premio con signo"
-        placeholder="Premio con signo"
-        size="lg"
-        radius="md"
-        rightSection={<IconGift />}
-        {...form.getInputProps('prizeWithSign')}
-      />
-
-      <Switch
-        mt={15}
-        checked={isFirstSwitchOn}
-        onChange={(event) => setIsFirstSwitchOn(event.currentTarget.checked)}
-        color="teal"
-        label="Rifa de dinero"
-        size="lg"
-      />
-
-      <Grid>
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-          <TextInput
-            label="Placa"
-            placeholder="Placa"
-            size="lg"
-            radius="md"
-            rightSection={<IconCar />}
-            disabled={isFirstSwitchOn}
-            {...form.getInputProps('plate')}
-          />
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-          <NumberInput
-            hideControls
-            size="lg"
-            radius="md"
-            label="Modelo"
-            placeholder="Modelo"
-            disabled={isFirstSwitchOn}
-            {...form.getInputProps('model')}
-          />
-        </Grid.Col>
-
-      </Grid>
-
-      <Switch
-        mt={10}
-        checked={isSecondSwitchOn}
-        color="teal"
-        label="Activar premio sin signo"
-        size="lg"
-      />
-      <TextInput
-        mt={10}
-        label="Premio sin signo"
-        placeholder="Premio sin signo (Opcional)"
-        size="lg"
-        radius="md"
-        rightSection={<IconGift />}
-        {...form.getInputProps('prizeWithoutSign')}
-      />
-      <Switch
-        mt={10}
-        checked={isSecondSwitchOn}
-        onChange={(event) => setIsSecondSwitchOn(event.currentTarget.checked)}
-        color="teal"
-        label="Rifa de dinero"
-        size="lg"
-      />
-      <Grid>
-
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-          <TextInput
-            label="Placa sin signo"
-            placeholder="Placa sin signo"
-            size="lg"
-            radius="md"
-            rightSection={<IconCar />}
-            disabled={isSecondSwitchOn}
-            {...form.getInputProps('plateWithoutSign')}
-          />
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-          <NumberInput
-            hideControls
-            size="lg"
-            radius="md"
-            label="Modelo sin signo"
-            placeholder="Modelo sin signo"
-            disabled={isSecondSwitchOn}
-            {...form.getInputProps('modelWithoutSign')}
-          />
-        </Grid.Col>
-      </Grid>
-
-
-
       <Select
-        mt={10}
-        label="Loteria"
-        placeholder="Loteria"
-        size="lg"
-        radius="md"
-        data={['Zulia 7A', 'Zulia 7B', 'Triple Pelotica']}
-        {...form.getInputProps('lottery')}
-      />
-
-      <Select
+        size={FIELDS_SIZE}
+        withAsterisk
         label="Moneda"
-        mt={10}
-        placeholder="Moneda"
-        size="lg"
-        radius="md"
-        data={['Dolares Americanos', 'Pesos Colombianos', 'Bolivares']}
+        placeholder="Ingrese una moneda"
+        data={CURRENCIES}
         {...form.getInputProps('currency')}
       />
-
-      <Grid mt={15}>
-        <Grid.Col span={6}>
-          <NumberInput
-            hideControls
-            size="lg"
-            radius="md"
-            label="Numeros"
-            placeholder="Numeros"
-            rightSection={<IconNumber />}
-            {...form.getInputProps('numbers')}
-          />
-        </Grid.Col>
-        <Grid.Col span={6}>
-          <NumberInput
-            hideControls
-            size="lg"
-            radius="md"
-            label="Precio"
-            placeholder="Precio"
-            rightSection={<IconCurrencyDollar />}
-            {...form.getInputProps('price')}
-          />
-        </Grid.Col>
-      </Grid>
-
+      <Group justify='space-between' mt={5}>
+        <NumberInput
+          type="tel"
+          withAsterisk
+          label="Precio"
+          size={FIELDS_SIZE}
+          w="calc(33.23% - 10px)"
+          placeholder="Precio"
+          rightSection={<IconCoin />}
+          {...form.getInputProps('price')}
+        />
+        <NumberInput
+          type="tel"
+          withAsterisk
+          size={FIELDS_SIZE}
+          w="calc(33.24% - 10px)"
+          rightSection={<Icon123 />}
+          label="Números"
+          placeholder="Números"
+          {...form.getInputProps('numbers')}
+        />
+        <Select
+          size={FIELDS_SIZE}
+          withAsterisk
+          w="calc(33.23% - 10px)"
+          label="Lotería"
+          placeholder="Lotería"
+          data={LOTERIES}
+          {...form.getInputProps('lotery')}
+        />
+      </Group>
       <Select
+        mt={5}
+        withAsterisk
         label="Rifero"
-        mt={10}
+        data={SELLERS}
+        size={FIELDS_SIZE}
         placeholder="Rifero"
-        size="lg"
-        radius="md"
-        data={['Evanan']}
-        {...form.getInputProps('rifero')}
+        {...form.getInputProps('seller_id')}
       />
-
       <Group justify="center" mt="xl">
         <Button variant="default" onClick={onBack}>Atras</Button>
         <Button type="submit" color="teal">Siguiente</Button>
